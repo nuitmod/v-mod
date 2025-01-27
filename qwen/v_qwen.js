8938ed7c613f22529fe86d1514cc2f60bc04884e
v_mod <wtvfqm@gmail.com>
2025-01-28 02:55:02 +0500

    add env

--- qwen/v_qwen.js
+++ qwen/v_qwen.js
@@ -1,16 +1,26 @@
-let url="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"
+let url
 let data=document.getElementById('input')
 console.log(data.value)
 let btn=document.getElementById('btn');
 //btn.addEventListener('click',()=>console.log(data.value))
-
+fetch('../config.json')
+            .then(response => response.json())
+            .then(config => {
+                console.log('API Key:', config.API_KEY);
+                url=config.API_KEY;
+                console.log(url)                
+            })
+            .catch(error => {
+                console.error('Ошибка при загрузке конфигурации:', error);
+            });
+           // console.log(url)
 function main(data_main){
 fetch("https://openrouter.ai/api/v1/chat/completions", {
     method: "POST",
     headers: {
       "Authorization": `Bearer ${url}`,
-      "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
-      "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
+      "HTTP-Referer": "https://v-mod.vercel.app/qwen/", // Optional. Site URL for rankings on openrouter.ai.
+      "X-Title": "v-mod", // Optional. Site title for rankings on openrouter.ai.
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
@@ -25,11 +35,12 @@ fetch("https://openrouter.ai/api/v1/chat/completions", {
   })
   .then(response => response.json())
   .then(data => {
+    let out=document.getElementById("output")
     if (data.error) {
       console.error('Ошибка:', data.error.message);
+      out.innerHTML=data.error.message
     } else {
       console.log('Ответ:', data.choices[0].message.content);
-      let out=document.getElementById("output")
       out.innerHTML=data.choices[0].message.content;
     }
   })
